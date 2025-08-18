5c00ab13edb3ed40428714384505daa23b5d3221
NikitaProgrammist <nikita.govorov.nikita@yandex.ru>
2025-08-18 15:36:26 +0300

    add \n in printf

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main() {
   printf("Введите коэффициенты a, b, c квадратного уравнения:\n");
   input = scanf("%lf%lf%lf", &square_equation.a, &square_equation.b, &square_equation.c);
   if (input != 3) {
-    printf("Некорректный ввод.");
+    printf("Некорректный ввод.\n");
     exit(0);
   }
   find_roots_square_equation(&square_equation);
